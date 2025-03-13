a6622d4e2b6356f8bfef8fd28e054d6c7a3775eb
bishwas350 <crashmindboy10@gmail.com>
2025-03-14 01:43:45 +0600

    socalpart add

---
+++ src/components/SocalPart.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Flex from './Flex'
+import facebook from '/public/facebook_5968764.png'
+import insta from '/public/insta.png'
+import tiktok from '/public/tiktok.png'
+import youtube from '/public/youtube.png'
+const SocalPart = () => {
+  return (
+    <>
+    <div className="w-full flex justify-center">
+        <Flex className={'items-center flex-wrap justify-center lg:justify-between gap-4 w-full'}>
+            <a href="https://www.facebook.com/share/1BVurQpyqG/" className='flex-1 flex justify-center'>
+                <img src={facebook} alt="facebook" className='h-[60px] md:h-[80px]' />
+            </a>
+                <a href="https://www.instagram.com/tu_sagor_sarker?igsh=MW1samFkeWp0OWFjeQ==" className='flex-1 flex justify-center'>
+                    <img src={insta} alt="" className='h-[80px] md:h-[100px]'/>
+                </a>
+                <a href="https://www.tiktok.com/@tusagorsarker?_t=ZS-8uefAfhFpPW&_r=1" className='flex-1 flex justify-center'>
+                <img src={tiktok} alt="" className='h-[60px] md:h-[80px]'/>
+                </a>
+                <a href="https://youtube.com/channel/UCXqo2VMhhyQ3LhQsHgPm92g?si=mGEqRksn-Cm3XQzw" className='flex-1 flex justify-center'>
+                    <img src={youtube} alt=""  className='h-[70px] md:h-[90px]'/>
+                </a>
+        </Flex>
+    </div>
+    </>
+  )
+}
+
+export default SocalPart
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from '@/components/Banner'
+import SocalPart from '@/components/SocalPart'
 import React from 'react'
 
 const Home = () => {
   return (
     <>
     <Banner/>
+    <SocalPart/>
     </>
   )
 }
