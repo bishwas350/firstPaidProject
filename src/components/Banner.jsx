12051f43afeb4b83b843f42346aa703a4656185a
bishwas350 <crashmindboy10@gmail.com>
2025-03-14 01:16:24 +0600

    banner add

---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='bg-bannerBg bg-cover bg-no-repeat bg-center py-20 md:py-40 lg:py-80 '></div>
+  )
+}
+
+export default Banner
