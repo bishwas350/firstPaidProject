9300a545c2a1ed201f8e5a8223a1d3388d7f6d17
bishwas350 <crashmindboy10@gmail.com>
2025-03-16 03:01:06 +0600

    some adjustment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,6 @@ import { IoMdClose } from "react-icons/io";
 import 'animate.css';
 import { Link } from 'react-router-dom';
 import { FaSearch } from "react-icons/fa";
-import ParticlesBackground from './ParticlesBackground';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -22,7 +21,7 @@ const Header = () => {
   return (
     <>
       <main>
-        <div className="bg-black py-5">
+        <div className="bg-black py-5 relative z-40">
           <Container>
             <Flex className={'justify-between items-center relative '}>
               {/* mobile part */}
@@ -35,8 +34,8 @@ const Header = () => {
                 </button>
               </div>
               {open && (
-                <div className={`absolute lg:hidden  top-11  animate__animated ${animateOut ? "animate__fadeOutLeft" : "animate__fadeInLeft"}`}>
-                  <div className="bg-gray-600 h-screen w-40  ">
+                <div className={`fixed lg:hidden left-0 top-[64px] z-50 animate__animated ${animateOut ? "animate__fadeOutLeft" : "animate__fadeInLeft"}`}>
+                  <div className="bg-gray-600 h-screen w-40">
                     <ul className='ml-10  pt-3'>
                       <Link to={"/"}>
                         <li className='font-mas text-black text-[12px] mb-5 leading-4  hover:font-semibold hover:text-white  hover:duration-200 cursor-pointer'>Home</li>
@@ -57,7 +56,6 @@ const Header = () => {
 
             </Flex>
             <div className="hidden lg:block">
-              
               <Flex className={'justify-between items-center'}>
                 <div  className="w-1/3">
                   <a href="/"><span className='text-white '>Sagor on fire</span></a>
