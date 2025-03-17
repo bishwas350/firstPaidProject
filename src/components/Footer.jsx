ca043fc874abb153bbb00149417e9c296388c55e
bishwas350 <crashmindboy10@gmail.com>
2025-03-17 23:32:05 +0600

    kisu na

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <ParticlesBackground />
       <div className="container mx-auto text-white text-center relative z-10 py-12">
         <h1 className='text-white text-center text-2xl font-mas font-semibold'>
-          Sagor <br /> on fire
+          Sagor <br /> on fire.
         </h1>
       </div>
     </footer>
